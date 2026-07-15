22cbf24eb6ec57ca3d57ae459e843d60b572a07f
pranavlonkar1202 <pranavlonkar53@gmail.com>
2026-07-15 16:40:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,3 +14,15 @@ This application predicts whether a credit card transaction is **Normal** or **F
 
 Upload a CSV file to begin.
 """)
+
+uploaded_file = st.file_uploader(
+    "Upload a CSV file",
+    type=["csv"]
+)
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader("Uploaded Dataset")
+
+    st.dataframe(df)
